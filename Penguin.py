50b8a1104ef205fb6e3a12c399e15d4c7687f518
Penguinblade1 <94222982+Penguinblade1@users.noreply.github.com>
2022-04-10 20:14:49 +0100

    This is my python

---
+++ Penguin.py
@@ -0,0 +1,10 @@
+print("Hello Who is running this Python")
+x = input("Input Your Name")
+#lets say I input Bob
+print(x)
+#It should output Bob
+print("What Day is it")
+x = input("What Day Is It")
+print("Bye Thanks For doing this with me")
+x = input("Say Thanks for helping me")
+#Penguinblade
